4b8e69de67dbc73da211e5b9cf423be4403600f0
KevinStreetCoder <kevinisaackareithi@gmail.com>
2023-06-06 23:19:35 +0300

    Python commits for loop and if-else

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def pow(a, b):
     """computes a to the power of b.
-    
+
     Arguments:
         a {int} -- base value
         b {int} -- exponential value
-    
+
     Returns:
         int -- computation of a^b
     """
