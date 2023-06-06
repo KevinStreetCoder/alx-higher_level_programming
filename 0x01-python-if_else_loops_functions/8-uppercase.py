48046081e731f7c52066a0676271b60038660619
KevinStreetCoder <kevinisaackareithi@gmail.com>
2023-06-06 22:13:45 +0300

    Python commits for loop and if-else

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,10 @@
+#!/usr/bin/python3
+
 def uppercase(str):
-    for c in str:
-        if ord(c) >= 97 and ord(c) <= 122:
-            uppercase_char = chr(ord(c) - 32)
-        else:
-            uppercase_char = c
-        print("{}".format(uppercase_char), end="")
-    print()
+	for c in str:
+		if ord(c) >= 97 and ord(c) <= 122:
+			uppercase_char = chr(ord(c) - 32)
+		else:
+			uppercase_char = c
+			print("{}".format(uppercase_char), end="")
+	print()
