9bdb69f70ac30ac99594b6a59d900b83145158cf
KevinStreetCoder <kevinisaackareithi@gmail.com>
2023-06-06 22:50:42 +0300

    Python commits for loop and if-else

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-
-for i in range(ord('a'), ord('z') + 1):
-	print(chr(i), end='')
-
-print()
+for letter in range(ord('a'), ord('z') + 1):
+    print('{}'.format(chr(letter)), end='')
