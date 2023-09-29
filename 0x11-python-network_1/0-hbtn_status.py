006bfa6f983d0d4b82b217e43f7d1c63fecc4ad1
KevinStreetCoder <kevinisaackareithi@gmail.com>
2023-09-29 11:50:51 -0700

    python network #1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -10,8 +10,8 @@ if __name__ == "__main__":
     
     with urllib.request.urlopen(url) as response:
         body = response.read()
-        print("Body response:")
-        print("    - type:", type(body))
-        print("    - content:", body)
-        print("    - utf8 content:", body.decode("utf-8"))
+        print("Body response:$")
+        print("    - type:", type(body), "$")
+        print("    - content:", body,"$")
+        print("    - utf8 content:", body.decode("utf-8"),"$")
 
